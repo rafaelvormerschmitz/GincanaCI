191070042b4e9438d912fcf3e717e3310be382a5
Rafael <rafael.schmitz@aluno.sc.senac.br>
2019-04-17 19:30:09 -0300

    Feito getALL e o insert no Prova_model

--- application/models/Prova_model.php
+++ application/models/Prova_model.php
@@ -1,8 +1,15 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class Prova_model extends CI_Model {
 
+    public function getALL() {
+        $query = $this->db->get('prova');
+        return $query->result();
+    }
+
+    public function insert($pv = array()) {
+        $this->db->insert('prova', $pv);
+        return $this->db->affected_rows();
+    }
+
+}
